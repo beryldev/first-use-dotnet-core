7865d9489a46bc2e77b670aaf7ebe751e6216235
Sebastian <sebastianm187@live.com>
2016-08-16 21:25:45 +0200

    warehouse operation concept

--- src/Warehouse/Operations/BaseDocument.cs
+++ src/Warehouse/Operations/BaseDocument.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Collections.Generic;
 
 namespace Warehouse.Operations
 {
     public class BaseDocument : IOperationDocument
     {
         public DateTime OperationDate { get; set; }
+        
+    }
+
+
+    public class BaseDocumentLine : IOperationDocumentLine
+    {
+        public string ProductName { get; set; }
+
+        public string ProductCode { get; set; }
+
+        public string EAN { get; set; }
+
+        public string SKU { get; set; }
+
+        public decimal Quantity { get; set; }
+
+        public string Remarks { get; set; }
     }
 }
--- src/Warehouse/Operations/Delivery/DocumentDelivery.cs
+++ src/Warehouse/Operations/Delivery/DocumentDelivery.cs
@@ -1,9 +1,15 @@
 using System;
+using System.Collections.Generic;
 
 namespace Warehouse.Operations.Delivery
 {
     public class DocumentDelivery : BaseDocument
     {
-        
+        List<DocumentDeliveryLine> Lines { get; set; } = new List<DocumentDeliveryLine>();
+    }
+
+    public class DocumentDeliveryLine : BaseDocumentLine
+    {
+        public string DestinationAddress { get; set; }
     }
 }
