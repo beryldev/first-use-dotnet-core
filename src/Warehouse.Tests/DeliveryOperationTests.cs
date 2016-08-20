2d37e3cc97630b08dfab71417a2ca8d22109eb71
Sebastian <sebastianm187@live.com>
2016-08-20 13:23:10 +0200

    operations on base document

--- src/Warehouse.Tests/DeliveryOperationTests.cs
+++ src/Warehouse.Tests/DeliveryOperationTests.cs
@@ -1,4 +1,6 @@
+using System;
 using NUnit.Framework;
+using Warehouse.Documents;
 using Warehouse.Operations.Delivery;
 using Warehouse.Orders;
 
@@ -7,16 +9,6 @@ namespace Warehouse.Tests
     [TestFixture]
     public class DeliveryOperationTests
     {
-        [Test]
-        public void PerformOperationReturnOperationResult()
-        {
-            var operation = new DeliveryOperation();
-            
-            var result = operation.Perform();
-
-            Assert.IsNotNull(result);
-        }
-
         [Test]
         public void OperationCanBasedOnOrder()
         {
@@ -38,5 +30,80 @@ namespace Warehouse.Tests
 
             Assert.AreEqual(document, operation.BaseDocument);
         }
+
+        [Test]
+        public void CanAcccessDirectlyToBaseOrder()
+        {
+            var document = new Order();
+            var operation = new DeliveryOperation();
+
+            operation.BaseOrder = document;
+
+            Assert.AreEqual(document, operation.BaseDocument);
+        }
+
+        [Test]
+        public void CanAccessDirectlyToBaseDeliveryDocument()
+        {
+            var document = new DeliveryDocument();
+            var operation  = new DeliveryOperation();
+
+            operation.BaseDeliveryDocument = document;
+
+            Assert.AreEqual(document, operation.BaseDocument);
+        }
+
+        [Test]
+        public void BaseDocumentIsAlwaysLastSetDocumentOrderThenDeliveryDoc()
+        {
+            var operation = new DeliveryOperation();
+            var order = new Order();
+            var deliveryDoc = new DeliveryDocument();
+
+            operation.SetBaseDocument(order);
+            operation.SetBaseDocument(deliveryDoc);
+
+            Assert.AreEqual(deliveryDoc, operation.BaseDocument);
+
+        }
+
+        [Test]
+        public void BaseDocumentIsAlwaysLastSetDocumentDeliveryDocThenOrder()
+        {
+            var operation = new DeliveryOperation();
+            var order = new Order();
+            var deliveryDoc = new DeliveryDocument();
+
+            operation.SetBaseDocument(deliveryDoc);
+            operation.SetBaseDocument(order);
+
+            Assert.AreEqual(order, operation.BaseDocument);
+        }
+
+        [Test]
+        public void ThrowsExceptionWhenPerformOperationWithoutBaseDocument()
+        {
+            var operation = new DeliveryOperation();
+
+            Assert.Throws<InvalidOperationException>(()=>
+            {
+               operation.Perform(); 
+            });
+        }
+
+        [Test]
+        public void PerformOperationReturnOperationResult()
+        {
+            var operation = new DeliveryOperation();
+            var order = new Order();
+            operation.SetBaseDocument(order);
+            
+            var result = operation.Perform();
+
+            Assert.IsNotNull(result);
+        }
+
+
+    
     }
 }
--- src/Warehouse/Operations/Delivery/DeliveryOperation.cs
+++ src/Warehouse/Operations/Delivery/DeliveryOperation.cs
@@ -7,13 +7,36 @@ namespace Warehouse.Operations.Delivery
 {
     public class DeliveryOperation
     {
+        public Document BaseDocument 
+        { 
+           get { return baseDocument; }
+        }
 
-        public Document BaseDocument { get { return baseDocument; } }
+        public Order BaseOrder 
+        { 
+            get 
+            { 
+                return baseDocument.GetType() == typeof(Order) ? (Order)baseDocument : null; 
+            } 
+            set { baseDocument = value; }
+        }
 
-        Document baseDocument; 
+        public DeliveryDocument BaseDeliveryDocument
+        {
+            get 
+            { 
+                return baseDocument.GetType() == typeof(DeliveryDocument) ? (DeliveryDocument)baseDocument : null; 
+            }
+            set { baseDocument = value; }
+        }
+
+        Document baseDocument;
         
         public DeliveryOperationResult Perform()
         {
+            if(baseDocument == null)
+                throw new InvalidOperationException("Can't perform operation without base document");
+                
             return new DeliveryOperationResult();
         }
 
