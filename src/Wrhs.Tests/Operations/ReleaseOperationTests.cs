7bcbf470fe4866efc473109c838b7611177faafc
Sebastian <sebastianm187@live.com>
2016-10-09 21:02:32 +0200

    release

--- src/Wrhs.Tests/Operations/ReleaseOperationTests.cs
+++ src/Wrhs.Tests/Operations/ReleaseOperationTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Moq;
 using NUnit.Framework;
 using Wrhs.Documents;
@@ -56,12 +57,89 @@ namespace Wrhs.Tests
         }
 
         [Test]
-        public void CantReleaseMoreThanOnBaseDocument()
+        public void CantReleaseAtOneTimeMoreThanOnBaseDocument()
         {
             var document = MakeReleaseDocument();
             var operation = MakeReleaseOperation(document);
 
-            
+            Assert.Throws<ArgumentException>(()=>
+            {
+                operation.ReleaseItem(document.Lines[0].Product, "LOC-001-01", 2);
+            });
+
+            Assert.AreEqual(0, operation.PendingAllocations.Count);
+        }
+
+        [Test]
+        public void CantReleaseCombinedMoreThanOnBaseDocument()
+        {
+            var document = MakeReleaseDocument();
+            var operation = MakeReleaseOperation(document);
+
+            operation.ReleaseItem(document.Lines[0].Product, "LOC-001-01", 1);
+
+            Assert.Throws<ArgumentException>(()=>
+            {
+                operation.ReleaseItem(document.Lines[0].Product, "LOC-001-01", 1);
+            });
+
+            Assert.AreEqual(1, operation.PendingAllocations.Count);
+        }
+
+        [Test]
+        public void CantReleaseResourceNonPresentOnDocument()
+        {
+            var document = MakeReleaseDocument();
+            var operation = MakeReleaseOperation(document);
+            var product = new Product { Code = "PROD2", Name="Product 2", EAN="111222333222" };
+
+            Assert.Throws<ArgumentException>(()=>
+            {
+                operation.ReleaseItem(product, "LOC-001-01", 1);
+            });
+
+            Assert.AreEqual(0, operation.PendingAllocations.Count);
+        }
+
+        [Test]
+        public void CantReleaseFromLocationNonPresentAtDocument()
+        {
+            var document = MakeReleaseDocument();
+            var operation = MakeReleaseOperation(document);
+
+            Assert.Throws<ArgumentException>(()=>
+            {
+                operation.ReleaseItem(document.Lines[0].Product, "LOC-001-004", 1);
+            });
+
+             Assert.AreEqual(0, operation.PendingAllocations.Count);
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(-1)]
+        [TestCase(-0.001)]
+        [TestCase(-100)]
+        public void CantReleaseZeroOrLess(decimal quantity)
+        {
+            var document = MakeReleaseDocument();
+            var operation = MakeReleaseOperation(document);
+
+            Assert.Throws<ArgumentException>(()=>
+            {
+                operation.ReleaseItem(document.Lines[0].Product, "LOC-001-01", quantity);
+            });
+
+            Assert.AreEqual(0, operation.PendingAllocations.Count);
+        }
+
+        [Test]
+        public void CantPerformWhenExistsNonRelocatedItems()
+        {
+            var items = new List<Allocation>();
+            var mock = new Mock<IAllocationService>();
+            var document = MakeReleaseDocument();
+            var operation = MakeReleaseOperation(document);
         }
 
         protected ReleaseDocument MakeReleaseDocument()
@@ -97,6 +175,11 @@ namespace Wrhs.Tests
             var operation = new ReleaseOperation();
 
             return operation;
+        }
+
+        protected Mock<IAllocationService> MakeAllocationServiceMock(List<Allocation> items)
+        {
+            
         }
     }
 }
