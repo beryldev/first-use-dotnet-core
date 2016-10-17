c558b67e2b37235e4f152f4740e86862f9875eb9
Sebastian <sebastianm187@live.com>
2016-10-17 21:15:07 +0200

    AllocationServiceTests refactoring

--- src/Wrhs.Tests/AllocationServiceTests.cs
+++ src/Wrhs.Tests/AllocationServiceTests.cs
@@ -19,9 +19,9 @@ namespace Wrhs.Tests
         [TestCase(null)]
         public void ThrowsExceptionWhenTryRegisterAllocationWithEmptyLocationName(string location)
         {
-            var mock = new Mock<IRepository<Allocation>>();
+            var repo = RepositoryFactory<Allocation>.Make();
             var allocation = MakeAllocation("PROD1", 1, location);
-            var service = MakeService(mock.Object);
+            var service = MakeService(repo);
 
             Assert.Throws<ArgumentException>(()=>{
                 service.RegisterAllocation(allocation);
@@ -35,10 +35,9 @@ namespace Wrhs.Tests
         [TestCase(null)]
         public void ThrowsExceptionWhenTryRegisterAllocationWithoutProductCode(string code)
         {
-            var items = new List<Allocation>();
-            var mock = PrepareAllocRepoMock(items);
+            var repo = RepositoryFactory<Allocation>.Make();
             var allocation = MakeAllocation(code);
-            var service = MakeService(mock.Object);
+            var service = MakeService(repo);
 
             Assert.Throws<ArgumentException>(()=>{
                 service.RegisterAllocation(allocation);
@@ -52,9 +51,9 @@ namespace Wrhs.Tests
         [TestCase(null)]
         public void ThrowsExceptionWhenTryRegisterDeallocationWithEmptyLocationName(string location)
         {
-            var mock = new Mock<IRepository<Allocation>>();
+            var repo = RepositoryFactory<Allocation>.Make();
             var allocation = MakeDeallocation("PROD1", -1, location);
-            var service = MakeService(mock.Object);
+            var service = MakeService(repo);
 
             Assert.Throws<ArgumentException>(()=>{
                 service.RegisterDeallocation(allocation);
@@ -68,10 +67,9 @@ namespace Wrhs.Tests
         [TestCase(null)]
         public void ThrowsExceptionWhenTryRegisterDeallocationWithoutProductCode(string code)
         {
-            var items = new List<Allocation>();
-            var mock = PrepareAllocRepoMock(items);
+            var repo = RepositoryFactory<Allocation>.Make();
             var allocation = MakeDeallocation(code);
-            var service = MakeService(mock.Object);
+            var service = MakeService(repo);
 
             Assert.Throws<ArgumentException>(()=>{
                 service.RegisterDeallocation(allocation);
@@ -81,81 +79,80 @@ namespace Wrhs.Tests
         [Test]
         public void RegisterSaveAllocationInRepository()
         {
-            var items = new List<Allocation>();
-            var mock = PrepareAllocRepoMock(items);
+            var repo = RepositoryFactory<Allocation>.Make();
             var allocation = MakeAllocation("PROD1");
-            var service = MakeService(mock.Object);
+            var service = MakeService(repo);
 
             service.RegisterAllocation(allocation);
 
-            mock.Verify(m=>m.Save(allocation), Times.Once());
-            CollectionAssert.Contains(items, allocation);
+            CollectionAssert.Contains(repo.Get().ToArray(), allocation);
         }
 
         [Test]
         public void RegisterSaveDeallocationInRepository()
         {
             var alloc = MakeAllocation("PROD1");
-            var items = new List<Allocation>{alloc};
-            var mock = PrepareAllocRepoMock(items);
-            var service = MakeService(mock.Object);
+            var repo = RepositoryFactory<Allocation>.Make();
+            repo.Save(alloc);
+            var service = MakeService(repo);
             var deallocation = MakeDeallocation("PROD1");
 
             service.RegisterDeallocation(deallocation);
 
-            mock.Verify(m=>m.Save(deallocation), Times.Once());
-            CollectionAssert.Contains(items, deallocation);
+            CollectionAssert.Contains(repo.Get().ToArray(), deallocation);
         }
 
         [Test]
         public void CantRegisterAllocationWithNegativeQuantity()
         {
-            var items = new List<Allocation>();
-            var mock = PrepareAllocRepoMock(items);
+            var repo = RepositoryFactory<Allocation>.Make();
             var allocation = MakeDeallocation("PROD1");
-            var service = MakeService(mock.Object);
+            var service = MakeService(repo);
 
             Assert.Throws<InvalidOperationException>(()=>
             {
                 service.RegisterAllocation(allocation);
             });
-            Assert.AreEqual(0, items.Count);
+
+            CollectionAssert.IsEmpty(repo.Get().ToArray());
         }
 
         [Test]
         public void CantRegisterDeallocationWithPositiveQuantity()
         {
-            var items = new List<Allocation>();
-            var mock = PrepareAllocRepoMock(items);
+            var repo = RepositoryFactory<Allocation>.Make();
             var allocation = MakeAllocation("PROD1");
-            var service = MakeService(mock.Object);
+            var service = MakeService(repo);
 
             Assert.Throws<InvalidOperationException>(()=>
             {
                 service.RegisterDeallocation(allocation);
             });
-            Assert.AreEqual(0, items.Count);
+
+            CollectionAssert.IsEmpty(repo.Get().ToArray());
         }
 
         [Test]
         public void GetAllocationsReturnAllocations()
         {
-            var items = new List<Allocation>{MakeAllocation("PROD1"), MakeAllocation("PROD2")};
-            var mock = PrepareAllocRepoMock(items);
-            var service = MakeService(mock.Object);
+            var repo = RepositoryFactory<Allocation>.Make();
+            repo.Save(MakeAllocation("PROD1"));
+            repo.Save(MakeAllocation("PROD2"));
+            var service = MakeService(repo);
 
             var result = service.GetAllocations();
 
-            Assert.AreEqual(items.Count, result.Count());
-            CollectionAssert.AreEquivalent(items, result);
+            Assert.AreEqual(repo.Get().Count(), result.Count());
+            CollectionAssert.AreEquivalent(repo.Get().ToArray(), result);
         }
 
         [Test]
         public void GetAllocationsReturnRegisteredAllocation()
         {
-            var items = new List<Allocation>{MakeAllocation("PROD1"), MakeAllocation("PROD2")};
-            var mock = PrepareAllocRepoMock(items);
-            var service = MakeService(mock.Object);
+            var repo = RepositoryFactory<Allocation>.Make();
+            repo.Save(MakeAllocation("PROD1"));
+            repo.Save(MakeAllocation("PROD2"));
+            var service = MakeService(repo);
             var allocation = MakeAllocation("PROD3");
 
             service.RegisterAllocation(allocation);
@@ -167,9 +164,9 @@ namespace Wrhs.Tests
         [Test]
         public void CantRegisterDeallocationWhenResourceNotExistsOnLocation()
         {
-            var items = new List<Allocation>{MakeAllocation("PROD1")};
-            var mock = PrepareAllocRepoMock(items);
-            var service = MakeService(mock.Object);
+            var repo = RepositoryFactory<Allocation>.Make();
+            repo.Save(MakeAllocation("PROD1"));
+            var service = MakeService(repo);
             var dealloc = MakeDeallocation("PROD1");
             dealloc.Location = "LOC-001-04";
 
@@ -177,79 +174,66 @@ namespace Wrhs.Tests
             {
                 service.RegisterDeallocation(dealloc);
             });
-            Assert.AreEqual(1, items.Count);
+            Assert.AreEqual(1, repo.Get().Count());
         }
 
         [Test]
         public void TransactionalRegisterAllocations()
         {
-            var items = new List<Allocation>{ MakeAllocation("PROD1") };
-            var mock = PrepareAllocRepoMock(items);
-            var service = MakeService(mock.Object);
+            var repo = RepositoryFactory<Allocation>.Make();
+            repo.Save(MakeAllocation("PROD1"));
+            var service = MakeService(repo);
 
             service.BeginTransaction();
             service.RegisterAllocation(MakeAllocation("PROD1", 2));
             service.CommitTransaction();
             
-            Assert.AreEqual(2, items.Count);
-            Assert.AreEqual(3, items.Sum(item=>item.Quantity));
+            Assert.AreEqual(2, repo.Get().Count());
+            Assert.AreEqual(3, repo.Get().Sum(item=>item.Quantity));
         }
 
         [Test]
         public void TransactionalRegisterDeallocations()
         {
-            var items = new List<Allocation>{ MakeAllocation("PROD1") };
-            var mock = PrepareAllocRepoMock(items);
-            var service = MakeService(mock.Object);
+            var repo = RepositoryFactory<Allocation>.Make();
+            repo.Save(MakeAllocation("PROD1"));
+            var service = MakeService(repo);
 
             service.BeginTransaction();
             service.RegisterDeallocation(MakeAllocation("PROD1", -1));
             service.CommitTransaction();
             
-            Assert.AreEqual(2, items.Count);
-            Assert.AreEqual(0, items.Sum(item=>item.Quantity));
+            Assert.AreEqual(2, repo.Get().Count());
+            Assert.AreEqual(0, repo.Get().Sum(item=>item.Quantity));
         }
 
         [Test]
         public void UncomittedTransactionNotChangeRepo()
         {
-            var items = new List<Allocation>{ MakeAllocation("PROD1") };
-            var mock = PrepareAllocRepoMock(items);
-            var service = MakeService(mock.Object);
+            var repo = RepositoryFactory<Allocation>.Make();
+            repo.Save(MakeAllocation("PROD1"));
+            var service = MakeService(repo);
 
             service.BeginTransaction();
             service.RegisterAllocation(MakeAllocation("PROD1", 2));
             
-            Assert.AreEqual(1, items.Count);
-            Assert.AreEqual(1, items.Sum(item=>item.Quantity));
+            Assert.AreEqual(1, repo.Get().Count());
+            Assert.AreEqual(1, repo.Get().Sum(item=>item.Quantity));
         }
 
         [Test]
         public void RolledbackTransactionNotChangeRepo()
         {
-            var items = new List<Allocation>{ MakeAllocation("PROD1") };
-            var mock = PrepareAllocRepoMock(items);
-            var service = MakeService(mock.Object);
+            var repo = RepositoryFactory<Allocation>.Make();
+            repo.Save(MakeAllocation("PROD1"));
+            var service = MakeService(repo);
 
             service.BeginTransaction();
             service.RegisterAllocation(MakeAllocation("PROD1", 2));
             service.RollbackTransaction();
             
-            Assert.AreEqual(1, items.Count);
-            Assert.AreEqual(1, items.Sum(item=>item.Quantity));
-        }
-
-        protected Mock<IRepository<Allocation>> PrepareAllocRepoMock(List<Allocation> items)
-        {
-            var mock = new Mock<IRepository<Allocation>>();
-
-            mock.Setup(m=>m.Save(It.IsAny<Allocation>()))
-                .Callback((Allocation alloc)=>{ items.Add(alloc); });
-
-            mock.Setup(m=>m.Get())
-                .Returns(items);
-
-            return mock;
+            Assert.AreEqual(1, repo.Get().Count());
+            Assert.AreEqual(1, repo.Get().Sum(item=>item.Quantity));
         }
 
         protected Allocation MakeAllocation(string code, decimal quantity=1, string location="LOC-001-01")
--- src/Wrhs.Tests/ProductRepositoryFactory.cs
+++ src/Wrhs.Tests/ProductRepositoryFactory.cs
@@ -44,4 +44,44 @@ namespace Wrhs.Tests
             return mock.Object;
         }
     }
+
+
+    public static class RepositoryFactory<T> where T : IEntity
+    {
+        public static IRepository<T> Make()
+        {
+             var items = new List<T>();
+            var mock = new Mock<IRepository<T>>();
+            mock.Setup(m=>m.Save(It.IsAny<T>()))
+                .Callback((T item)=>{ items.Add(item); });
+
+            mock.Setup(m=>m.Get())
+                .Returns(items);
+
+            mock.Setup(m=>m.GetById(It.IsAny<int>()))
+                .Returns((int id)=>
+                { 
+                    return items.ToArray().Where(item=>item.Id==id).FirstOrDefault(); 
+                });
+
+            mock.Setup(m=>m.Update(It.IsAny<T>()))
+                .Callback((T obj)=>
+                {
+                    var p = items.Where(item=>item.Id==obj.Id).FirstOrDefault();
+                    if(p!=null)
+                    {
+                        items.Remove(p);
+                        items.Add(obj);
+                    }
+                });
+
+            mock.Setup(m=>m.Delete(It.IsAny<T>()))
+                .Callback((T obj) =>
+                {
+                    items.Remove(obj);
+                });
+
+            return mock.Object;
+        }
+    }
 }
