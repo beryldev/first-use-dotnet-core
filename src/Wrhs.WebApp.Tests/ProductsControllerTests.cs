6e9b76d16f9827052a1342da44d212bf6b9d16a7
Sebastian <sebastianm187@live.com>
2016-10-29 19:05:25 +0200

    products controller

---
+++ src/Wrhs.WebApp.Tests/ProductsControllerTests.cs
@@ -0,0 +1,231 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Wrhs.Core;
+using Wrhs.Products;
+using Wrhs.Products.Commands;
+using Wrhs.WebApp.Controllers;
+using Xunit;
+
+namespace Wrhs.WebApp.Tests
+{
+    public class ProductsControllerTests
+    {
+        [Fact]
+        public void ShouldReturnAllProductsAsPaginateResultWhenNoRequestParams()
+        {
+            var repository = RepositoryFactory<Product>.Make();
+            repository.Save(new Product{Code="PROD1", Name="Product 1", EAN = "1111"});
+            var controller = new ProductsController(repository);
+
+            var result = controller.Get();
+
+            Assert.Equal(1, result.Total);
+        }
+
+        [Fact]
+        public void ShouldReturnFilteredResultWhenPassNameRequestParam()
+        {
+            var repository = RepositoryFactory<Product>.Make();
+            repository.Save(new Product{Code="PROD1", Name="Product 1", EAN = "1111"});
+            var controller = new ProductsController(repository);
+
+            var result = controller.Get(name:"Product 1");
+
+            Assert.Equal(1, result.Total);
+        }
+
+        [Fact]
+        public void ShouldReturnFilteredResultWhenPassCodeRequestParam()
+        {
+            var repository = RepositoryFactory<Product>.Make();
+            repository.Save(new Product{Code="PROD1", Name="Product 1", EAN = "1111"});
+            var controller = new ProductsController(repository);
+
+            var result = controller.Get(code:"PROD1");
+
+            Assert.Equal(1, result.Total);
+        }
+
+        [Fact]
+        public void ShouldReturnFilteredResultWhenPassEANRequestParam()
+        {
+            var repository = RepositoryFactory<Product>.Make();
+            repository.Save(new Product{Code="PROD1", Name="Product 1", EAN = "1111"});
+            var controller = new ProductsController(repository);
+
+            var result = controller.Get(ean:"1111");
+
+            Assert.Equal(1, result.Total);
+        }
+
+        [Fact]
+        public void ShouldReturnProductWhenGetExistedProduct()
+        {
+            var product = new Product(){Code = "PROD1", Name = "Product 1"};
+            var repository = new Mock<IRepository<Product>>();
+            repository.Setup(m=>m.GetById(It.IsAny<int>()))
+                .Returns(product);
+            var controller = new ProductsController(repository.Object);
+
+            var result = controller.GetById(1);
+
+            Assert.Equal("PROD1", result.Code);
+            Assert.Equal("Product 1", result.Name);
+        }
+
+        [Fact]
+        public void ShouldReturnEmptyProductWhenGetNonExistedProduct()
+        {
+            var repository = new Mock<IRepository<Product>>();
+            repository.Setup(m=>m.GetById(It.IsAny<int>()))
+                .Returns((Product)null);
+            var controller = new ProductsController(repository.Object);
+
+            var result = controller.GetById(1);
+
+            Assert.Equal(0, result.Id);
+            Assert.Null(result.Code);
+            Assert.Null(result.Name);
+        }
+
+        [Fact]
+        public void ShoudReturnOkRequestOnSuccessWhenCreateProduct()
+        {
+            var repository = RepositoryFactory<Product>.Make();
+            var cmd = new CreateProductCommand();
+            var handler = new Mock<ICommandHandler<CreateProductCommand>>();
+            var validator = new Mock<IValidator<CreateProductCommand>>();
+            validator.Setup(m=>m.Validate(cmd)).Returns(new List<ValidationResult>());
+            var controller = new ProductsController(repository);
+
+            var result = controller.CreateProduct(cmd, validator.Object, handler.Object);
+
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public void ShoudBadRequestWithErrorsRequestOnFailWhenCreateProduct()
+        {
+            var repository = RepositoryFactory<Product>.Make();
+            var cmd = new CreateProductCommand();
+            var handler = new Mock<ICommandHandler<CreateProductCommand>>();
+            var validator = new Mock<IValidator<CreateProductCommand>>();
+            validator.Setup(m=>m.Validate(cmd))
+                .Returns(new List<ValidationResult>(){new ValidationResult{Field="test", Message="test"}});
+            var controller = new ProductsController(repository);
+
+            var result = controller.CreateProduct(cmd, validator.Object, handler.Object);
+
+            Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal(1, ((IEnumerable<ValidationResult>)((BadRequestObjectResult)result).Value).Count());
+        }
+
+        [Fact]
+        public void ShouldReturnOkOnDeleteWhenSuccess()
+        {
+            var repository = new Mock<IRepository<Product>>();
+              repository.Setup(m=>m.GetById(It.IsAny<int>()))
+                .Returns(new Product());
+            var cmd = new DeleteProductCommand();
+            var handler = new Mock<ICommandHandler<DeleteProductCommand>>();
+            var validator = new Mock<IValidator<DeleteProductCommand>>();
+            validator.Setup(m=>m.Validate(cmd))
+                .Returns(new List<ValidationResult>());
+            var controller = new ProductsController(repository.Object);
+
+            var result = controller.Delete(1, cmd, validator.Object, handler.Object);
+
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public void ShouldReturnBadRequestOnDeleteWhenFail()
+        {
+            var repository = new Mock<IRepository<Product>>();
+              repository.Setup(m=>m.GetById(It.IsAny<int>()))
+                .Returns(new Product());
+            var cmd = new DeleteProductCommand();
+            var handler = new Mock<ICommandHandler<DeleteProductCommand>>();
+            var validator = new Mock<IValidator<DeleteProductCommand>>();
+            validator.Setup(m=>m.Validate(cmd))
+                .Returns(new List<ValidationResult>(){new ValidationResult()});
+            var controller = new ProductsController(repository.Object);
+
+            var result = controller.Delete(1, cmd, validator.Object, handler.Object);
+
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public void ShouldReturnOkOnUpdateWhenSuccess()
+        {
+            var repository = new Mock<IRepository<Product>>();
+              repository.Setup(m=>m.GetById(It.IsAny<int>()))
+                .Returns(new Product());
+            var cmd = new UpdateProductCommand();
+            var handler = new Mock<ICommandHandler<UpdateProductCommand>>();
+            var validator = new Mock<IValidator<UpdateProductCommand>>();
+            validator.Setup(m=>m.Validate(cmd))
+                .Returns(new List<ValidationResult>());
+            var controller = new ProductsController(repository.Object);
+
+            var result = controller.Update(1, cmd, validator.Object, handler.Object);
+
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public void ShouldReturnBadRequestOnUpdateWhenFail()
+        {
+            var repository = new Mock<IRepository<Product>>();
+              repository.Setup(m=>m.GetById(It.IsAny<int>()))
+                .Returns(new Product());
+            var cmd = new UpdateProductCommand();
+            var handler = new Mock<ICommandHandler<UpdateProductCommand>>();
+            var validator = new Mock<IValidator<UpdateProductCommand>>();
+            validator.Setup(m=>m.Validate(cmd))
+                .Returns(new List<ValidationResult>(){new ValidationResult()});
+            var controller = new ProductsController(repository.Object);
+
+            var result = controller.Update(1, cmd, validator.Object, handler.Object);
+
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public void ShouldReturnStocksWhenGetStocks()
+        {
+            var repository = new Mock<IRepository<Product>>();
+            repository.Setup(m=>m.GetById(It.IsAny<int>()))
+                .Returns(new Product());
+            var warehouse = new Mock<IWarehouse>();
+            warehouse.Setup(m=>m.ReadStocksByProductCode(It.IsAny<string>()))
+                .Returns(new List<Stock>());
+            var controller = new ProductsController(repository.Object);
+
+            var result = controller.GetStocks(1, warehouse.Object);
+
+            Assert.IsType<List<Stock>>(result);
+        }
+
+        [Fact]
+        public void ShouldReturnStocksWhenGetCalculatedStocks()
+        {
+            var repository = new Mock<IRepository<Product>>();
+            repository.Setup(m=>m.GetById(It.IsAny<int>()))
+                .Returns(new Product());
+            var warehouse = new Mock<IWarehouse>();
+            warehouse.Setup(m=>m.CalculateStocks(It.IsAny<string>()))
+                .Returns(new List<Stock>());
+            var controller = new ProductsController(repository.Object);
+
+            var result = controller.GetCalculatedStocks(1, warehouse.Object);
+
+            Assert.IsType<List<Stock>>(result);
+        }
+    }
+    
+}
--- src/Wrhs.WebApp/Controllers/ProductsController.cs
+++ src/Wrhs.WebApp/Controllers/ProductsController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Wrhs.Core;
@@ -19,23 +20,57 @@ namespace Wrhs.WebApp.Controllers
         }
 
         [HttpGet]
-        public IPaginateResult<Product> Get()
+        public IPaginateResult<Product> Get(string name="", string code="",  string ean="")
         {
             var paginator = new Paginator<Product>();
             var search = new ResourceSearch<Product>(productRepository, paginator, new ProductSearchCriteriaFactory());
             var criteria = (ProductSearchCriteria)search.MakeCriteria();
+            
+            if(!String.IsNullOrWhiteSpace(name))
+                criteria.WhereName(Condition.Contains, name);
+
+            if(!String.IsNullOrWhiteSpace(code))
+                criteria.WhereCode(Condition.Contains, code);
+
+            if(!String.IsNullOrWhiteSpace(ean))
+                criteria.WhereEAN(Condition.Contains, ean);
+
             return search.Exec(criteria);
         }
 
+        [HttpGet("{id}")]
+        public Product GetById(int id)
+        {
+            var product = productRepository.GetById(id);
+
+            return product == null ? new Product() : product;
+        }
+
         [HttpPost]
-        public IActionResult Post([FromBody]CreateProductCommand cmd, [FromServices]IValidator<CreateProductCommand> validator, 
+        public IActionResult CreateProduct([FromBody]CreateProductCommand cmd, [FromServices]IValidator<CreateProductCommand> validator, 
             [FromServices]ICommandHandler<CreateProductCommand> handler)
         {            
             return HandleCommand<CreateProductCommand>(handler, validator, cmd);
         }
 
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id, [FromBody] DeleteProductCommand cmd, 
+            [FromServices]IValidator<DeleteProductCommand> validator, [FromServices]ICommandHandler<DeleteProductCommand> handler)
+        {
+            cmd.ProductId = id;
+            return HandleCommand<DeleteProductCommand>(handler, validator, cmd);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] UpdateProductCommand cmd, 
+            [FromServices]IValidator<UpdateProductCommand> validator, [FromServices]ICommandHandler<UpdateProductCommand> handler)
+        {
+            cmd.ProductId = id;
+            return HandleCommand<UpdateProductCommand>(handler, validator, cmd);
+        }
+
         [HttpGet("{productId}/stocks")]
-        public IEnumerable<Stock> Stocks(int productId, [FromServices]IWarehouse warehouse)
+        public IEnumerable<Stock> GetStocks(int productId, [FromServices]IWarehouse warehouse)
         {
             var product = productRepository.GetById(productId);
 
@@ -44,7 +79,7 @@ namespace Wrhs.WebApp.Controllers
         }
 
         [HttpGet("{productId}/stocks/calculated")]
-        public IEnumerable<Stock> CalculatedStocks(int productId, [FromServices]IWarehouse warehouse)
+        public IEnumerable<Stock> GetCalculatedStocks(int productId, [FromServices]IWarehouse warehouse)
         {
             var product = productRepository.GetById(productId);
 
