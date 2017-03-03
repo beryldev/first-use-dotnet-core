99f158cda7d55260ee7e9ed6e5e6be779668b6aa
Sebastian <sebastianm187@live.com>
2017-03-03 21:03:29 +0100

    przemeblowanie

--- src/Wrhs.WebApp/Startup.cs
+++ src/Wrhs.WebApp/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using Wrhs.Common;
 using Wrhs.Core;
@@ -38,6 +40,15 @@ namespace Wrhs.WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
+            services.AddDbContext<IdentityDbContext>(options => 
+                options.UseSqlite("Data Source=users.sqlite", 
+            optionsBuilder => optionsBuilder.MigrationsAssembly("Wrhs.WebApp")));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<IdentityDbContext>()
+                .AddDefaultTokenProviders();
+
             // Add framework services.
             services.AddMvc().AddJsonOptions(options => {
                 //options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
@@ -50,8 +61,14 @@ namespace Wrhs.WebApp
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IdentityDbContext dbContext)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                dbContext.Database.Migrate(); //this will generate the db if it does not exist
+            }
+            app.UseIdentity();
             app.UseStaticFiles();
 
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
