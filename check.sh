fce22be483ef6ce43bd808b2d8bb9c6428d2e1f5
Sebastian <sebastianm187@live.com>
2017-03-06 21:19:34 +0100

    check

--- check.sh
+++ check.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
+error=0;
+projects=(
+    './Wrhs.Tests/Wrhs.Tests.csproj'
+    './Wrhs.Data.Tests/Wrhs.Data.Tests.csproj'
+    './Wrhs.WebApp.Tests/Wrhs.WebApp.Tests.csproj'
+)
+
+./build.sh;
+if [ $? -ne 0 ]; then error=1; fi;
 
 pushd ./src;
-dotnet restore;
-dotnet build;
-dotnet test ./Wrhs.Tests/Wrhs.Tests.csproj;
-dotnet test ./Wrhs.Data.Tests/Wrhs.Data.Tests.csproj;
-dotnet test ./Wrhs.WebApp.Tests/Wrhs.WebApp.Tests.csproj;
+
+count=0;
+while [ "x${projects[count]}" != "x" ]
+do
+    echo "${projects[count]}";
+    dotnet test "${projects[count]}";
+    if [ $? -ne 0 ]; then error=1; fi;
+    count=$(( $count + 1 ))
+done
+
 popd;
+exit $error;
